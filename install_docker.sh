1abbf23c9a2ced0bae40bc88f30976172b182bd4
jcdavenport <34253710+jcdavenport@users.noreply.github.com>
2018-09-17 11:39:18 -0500

    Add files via upload

---
+++ install_docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common \
+    mousepad \
+    xfonts-terminus \
+    darkcold-gtk-theme
+	
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic edge"
+   
+sudo apt-get update
+
+sudo apt-get install -y docker-ce
+
+sudo usermod -aG docker $USER
+
+#refresh group assignment
+exec su -l $USER
+
+#restart lxde
+lxpanelctl restart && openbox --restart
